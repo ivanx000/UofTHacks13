b8d848f85f00c38774dd203b94fdf77471a6b7a2
Grace Wang <84159791+Grace273@users.noreply.github.com>
2026-01-17 11:42:55 -0500

    created product cards

--- frontend/app/suggested_products/page.tsx
+++ frontend/app/suggested_products/page.tsx
@@ -1,7 +1,19 @@
-export default function SuggestionsPage() {
+import ProductCard from "@/components/suggested_product_card/SuggestedProductCard";
+
+export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-4xl">
-      Suggestions Page
+    <div className="p-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <ProductCard
+        title="Handmade Knit Scarf"
+        description="Cozy, handmade scarf perfect for winter."
+        price={45}
+      />
+
+      <ProductCard
+        title="Crochet Beanie"
+        description="Soft and warm beanie made with love."
+        price={30}
+      />
     </div>
   );
 }
